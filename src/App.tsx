16dac56d37556032e525e78e6ad6981ace2b79dc
Mohamed Hassen Mami <mohamed@paraswap.io>
2022-11-07 21:03:36 +0100

    handle network mismatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import { useAccount } from "wagmi";
+import { useAccount, useNetwork, useSwitchNetwork } from "wagmi";
 import "./App.css";
 import Claimer from "./Claimer";
 import WalletConnect from "./WalletConnect";
 
+const supportedChainId = 5;
+
 function App() {
   const { isConnected } = useAccount();
+  const { chain } = useNetwork();
+  const { switchNetwork, chains } = useSwitchNetwork();
+  const supportedChain = chains.find((e) => e.id === supportedChainId);
 
   return (
     <div>
@@ -13,6 +18,19 @@ function App() {
           <h1>Claim Your ParaSwapPool 4 & 10 Compensation</h1>
           <WalletConnect />
         </>
+      ) : chain?.id != supportedChainId ? (
+        <h3
+          className="error"
+          style={{ display: "flex", alignItems: "center", columnGap: 10 }}
+        >
+          Wrong network please switch to {supportedChain?.name}{" "}
+          <button
+            style={{ fontSize: 20, backgroundColor: "white", color: "red" }}
+            onClick={() => switchNetwork?.(supportedChainId)}
+          >
+            switch
+          </button>
+        </h3>
       ) : (
         <Claimer />
       )}
