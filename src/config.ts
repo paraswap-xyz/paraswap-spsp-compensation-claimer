8823362a72e21b0815d7e284093578f83e71fc7f
Mohamed Hassen Mami <mohamed@paraswap.io>
2022-11-08 23:21:36 +0100

    set temp address to test

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import { Config } from "./types";
 
 export default {
   1: {
-    claimer: "0xb7c74317fe4d0cabd414430d8253af087152a5f3",
+    claimer: "0x7f4174A90B4363556ee089Fe5fffAaF26352fc72", // to test BUT actual 0xb7c74317fe4d0cabd414430d8253af087152a5f3
     ipfsHash: "QmX8CWo43RD5gY85kb96GWSkPF8A6FbPxTJWmZGxYCqC4H",
   },
   5: {
